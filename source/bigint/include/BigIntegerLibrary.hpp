9efba5559904cda1250e7c68f3633ea7ab235f35
Iason Papadopoulos <jason099080@hotmail.com>
2018-10-06 21:04:12 +0100

    Replace the raw ninty-233 library with the packaged fork
    See https://github.com/jason0597/ninty-233

--- source/bigint/include/BigIntegerLibrary.hpp
+++
@@ -1,8 +0,0 @@
-// This header file includes all of the library header files.
-
-#include "NumberlikeArray.hpp"
-#include "BigUnsigned.hpp"
-#include "BigInteger.hpp"
-#include "BigIntegerAlgorithms.hpp"
-#include "BigUnsignedInABase.hpp"
-#include "BigIntegerUtils.hpp"
